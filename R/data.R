b3d6cd0ddee5e036e096157b6962763685a71e90
colinbrust <colin.brust@gmail.com>
2022-09-07 16:04:23 -0600

    Update data documentation.

--- R/data.R
+++ R/data.R
@@ -1,71 +1,71 @@
 #' Downscaled daily CMIP6 relative humidity (%) projections across Montana
-#' for the 2015-01-01 to 2100-12-30 record.
+#' for the 2015-01-01 to 2016-01-01 record.
 #'
 #' @format A `terra::rast` object with the following attributes:
 #' \describe{
 #'   \item{CRS}{EPSG 4326}
 #'   \item{resolution}{0.25 x 0.25 degrees}
-#'   \item{time}{A date list ranging from 2015-01-01 to 2100-12-30}
+#'   \item{time}{A date list ranging from 2015-01-01 to 2016-01-01}
 #' }
 #' @source \url{https://ds.nccs.nasa.gov/thredds/catalog/AMES/NEX/GDDP-CMIP6/catalog.html}
 "rh"
 
 #' Downscaled daily CMIP6 downwelling shortwave solar radiaiton (W m^-2)
-#' projections across Montana for the 2015-01-01 to 2100-12-30 record.
+#' projections across Montana for the 2015-01-01 to 2016-01-01 record.
 #'
 #' @format A `terra::rast` object with the following attributes:
 #' \describe{
 #'   \item{CRS}{EPSG 4326}
 #'   \item{resolution}{0.25 x 0.25 degrees}
-#'   \item{time}{A date list ranging from 2015-01-01 to 2100-12-30}
+#'   \item{time}{A date list ranging from 2015-01-01 to 2016-01-01}
 #' }
 #' @source \url{https://ds.nccs.nasa.gov/thredds/catalog/AMES/NEX/GDDP-CMIP6/catalog.html}
 "srad"
 
 #' Downscaled daily CMIP6 wind speed (m s^-1) projections across Montana for the
-#' 2015-01-01 to 2100-12-30 record.
+#' 2015-01-01 to 2016-01-01 record.
 #'
 #' @format A `terra::rast` object with the following attributes:
 #' \describe{
 #'   \item{CRS}{EPSG 4326}
 #'   \item{resolution}{0.25 x 0.25 degrees}
-#'   \item{time}{A date list ranging from 2015-01-01 to 2100-12-30}
+#'   \item{time}{A date list ranging from 2015-01-01 to 2016-01-01}
 #' }
 #' @source \url{https://ds.nccs.nasa.gov/thredds/catalog/AMES/NEX/GDDP-CMIP6/catalog.html}
 "ws"
 
 #' Downscaled daily CMIP6 mean temperature (K) projections across Montana for the
-#' 2015-01-01 to 2100-12-30 record.
+#' 2015-01-01 to 2016-01-01 record.
 #'
 #' @format A `terra::rast` object with the following attributes:
 #' \describe{
 #'   \item{CRS}{EPSG 4326}
 #'   \item{resolution}{0.25 x 0.25 degrees}
-#'   \item{time}{A date list ranging from 2015-01-01 to 2100-12-30}
+#'   \item{time}{A date list ranging from 2015-01-01 to 2016-01-01}
 #' }
 #' @source \url{https://ds.nccs.nasa.gov/thredds/catalog/AMES/NEX/GDDP-CMIP6/catalog.html}
 "tmean"
 
 #' Downscaled daily CMIP6 max temperature (K) projections across Montana for the
-#' 2015-01-01 to 2100-12-30 record.
+#' 2015-01-01 to 2016-01-01 record.
 #'
 #' @format A `terra::rast` object with the following attributes:
 #' \describe{
 #'   \item{CRS}{EPSG 4326}
 #'   \item{resolution}{0.25 x 0.25 degrees}
-#'   \item{time}{A date list ranging from 2015-01-01 to 2100-12-30}
+#'   \item{time}{A date list ranging from 2015-01-01 to 2016-01-01}
 #' }
 #' @source \url{https://ds.nccs.nasa.gov/thredds/catalog/AMES/NEX/GDDP-CMIP6/catalog.html}
 "tmax"
 
 #' Downscaled daily CMIP6 min temperature (K) projections across Montana for the
-#' 2015-01-01 to 2100-12-30 record.
+#' 2015-01-01 to 2016-01-01 record.
 #'
 #' @format A `terra::rast` object with the following attributes:
 #' \describe{
 #'   \item{CRS}{EPSG 4326}
 #'   \item{resolution}{0.25 x 0.25 degrees}
-#'   \item{time}{A date list ranging from 2015-01-01 to 2100-12-30}
+#'   \item{time}{A date list ranging from 2015-01-01 to 2016-01-01}
 #' }
 #' @source \url{https://ds.nccs.nasa.gov/thredds/catalog/AMES/NEX/GDDP-CMIP6/catalog.html}
 "tmin"
